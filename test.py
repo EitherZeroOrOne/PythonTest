009dc594749e8a4d572bc674cd96deccb7fdb256
EitherZeroOrOne <narekkaz@gmail.com>
2018-02-08 14:19:00 -0800

    Another tag

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 """ Example module docstring """
 def funcname():
-    """ Example function docstring blah """
+    """ Example function docstring blah blah """
     test_var = 5
     print test_var
 
